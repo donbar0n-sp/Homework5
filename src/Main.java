9486ae25af4e215bf7b61aa8d30b7f64fe6d003d
donbar0n-sp <v.dublyanskiy@gmail.com>
2024-10-09 21:05:25 +0700

    hw5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,90 @@
+@SuppressWarnings({"DataFlowIssue", "ConstantValue"})
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //task1
+        int clientOS = 1;
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println("Device not found, please enter correct operating system");
+        }
+
+        //task2
+        int ClientOS = 1;
+        int clientDeviceYear = 2015;
+        boolean newDevice = clientDeviceYear >= 2015;
+        if (!newDevice && ClientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        else if (newDevice && ClientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else if (!newDevice && ClientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        else if (newDevice && ClientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        else {
+            System.out.println("Device not found, please enter correct operating system");
+        }
+
+        //task3
+        int year = 2024;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        }
+        else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        //task4
+        int deliveryDistance = 95;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        }
+        else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: 2");
+        }
+        else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: 3");
+        }
+        else {
+            System.out.println("delivery not possible");
+        }
+
+        //task5
+        int month = 12;
+        switch (month) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("fall");
+                break;
+            default:
+                System.out.println("there is no such month");
+        }
+
     }
 }
